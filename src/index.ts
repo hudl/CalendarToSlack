d3d31e77a6cdc4397c4799b97c195bc56d8cdaa1
Jordan Degner <jdegner0129@gmail.com>
2019-07-15 16:25:47 -0500

    Set up sub-lambda to update batches of users

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { getEventsForUser, CalendarEvent } from './services/calendar';
-import { getAllUserSettings, UserSettings } from './services/dynamo';
+import { getAllUserSettings, UserSettings, getSettingsForUsers } from './services/dynamo';
 import { setSlackStatus, SlackStatus } from './services/slack';
+import AWS from 'aws-sdk';
+import { Handler } from 'aws-lambda';
 
 const getHighestPriorityEvent = (events: CalendarEvent[]) => {
   // TODO: Implement this function to resolve the event to use for status updates from a list of user events
@@ -21,10 +23,32 @@ const getStatusForUserEvent = (settings: UserSettings, event: CalendarEvent | nu
   return defaultAwayStatus;
 };
 
-export const handler = async (event: any) => {
-  const userSettings = await getAllUserSettings();
+export const update: Handler = async () => {
+  const batchSize = 10;
 
-  // TODO: Consider replacing this with code to start a separate lambda that runs for a smaller batch of users, depending on timing
+  const lambda = new AWS.Lambda({
+    apiVersion: 'latest',
+    region: 'us-east-1',
+    endpoint: process.env.IS_OFFLINE ? 'http://localhost:3000' : undefined,
+  });
+
+  const invokeParams = {
+    FunctionName: 'calendar2slack-dev-update-batch',
+    InvocationType: 'Event',
+    LogType: 'None',
+  };
+
+  const userSettings = await getAllUserSettings();
+  for (var i = 0; i < userSettings.length; i += batchSize) {
+    const batch = userSettings.slice(i, i + batchSize).map(us => us.email);
+
+    lambda.invoke({ Payload: JSON.stringify({ emails: batch }), ...invokeParams }).send();
+  }
+};
+
+export const updateBatch: Handler = async (event: any) => {
+  const userSettings = await getSettingsForUsers(event.emails);
+
   await Promise.all(
     userSettings.map(async us => {
       const userEvents = await getEventsForUser(us.email);
--- src/services/dynamo.ts
+++ src/services/dynamo.ts
@@ -2,8 +2,6 @@ import { SlackStatus } from './slack';
 import AWS from 'aws-sdk';
 import config from '../../config';
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
 export type UserSettings = {
   email: string;
   slackToken: string;
@@ -15,13 +13,45 @@ export type UserSettings = {
 };
 
 export const getAllUserSettings = async (): Promise<UserSettings[]> => {
-  return new Promise(resolve =>
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return new Promise((resolve, reject) =>
     dynamoDb.scan(
       {
         TableName: config.dynamoDb.tableName,
+        ProjectionExpression: 'email',
       },
-      (_, data) => {
+      (err, data) => {
+        if (err) {
+          reject(err.message);
+          return;
+        }
+
         resolve(data.Items as UserSettings[]);
+      },
+    ),
+  );
+};
+
+export const getSettingsForUsers = async (emails: string[]): Promise<UserSettings[]> => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return new Promise((resolve, reject) =>
+    dynamoDb.batchGet(
+      {
+        RequestItems: {
+          [config.dynamoDb.tableName]: { Keys: emails.map(email => ({ email })) },
+        },
+      },
+      (err, data) => {
+        if (err) {
+          reject(err.message);
+          return;
+        }
+
+        resolve(
+          data.Responses ? (data.Responses[config.dynamoDb.tableName] as UserSettings[]) : ([] as UserSettings[]),
+        );
       },
     ),
   );
