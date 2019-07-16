ba38bcf28bd70b497d066e078b23294e62f96e2e
Tyson Stewart <tyson.stewart@hudl.com>
2019-07-16 11:14:43 -0500

    Slackbot: first attempt at message verification

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,15 +1,77 @@
-export const handler = async (event: any) => {
-    console.log(JSON.stringify(event));
+import qs from "qs";
+import crypto from "crypto";
+import AWS from "aws-sdk";
+import config from "../config";
 
-    let body = JSON.parse(event.body);
+const MILLIS_IN_SEC = 1000;
+const FIVE_MIN_IN_SEC = 300;
 
-    // verify request
+type ApiGatewayEvent = {
+  headers: {
+    [header: string]: string;
+  };
+  body: string;
+};
 
-    // respond with challenge
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify({"challenge":body.challenge})
+async function getSigningSecret(): Promise<string> {
+  const client = new AWS.SecretsManager({
+    region: config.region
+  });
+
+  try {
+    const data = await client.getSecretValue({ SecretId: config.slack.secretName }).promise();
+    if ("SecretString" in data && data.SecretString) {
+      return data.SecretString;
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+
+  throw new Error("Slack signing secret not configured properly");
+}
+
+function validateTimestamp(slackRequestTimestampInSec: number): boolean {
+  const currentTimeInSec = Math.floor(new Date().getTime() / MILLIS_IN_SEC);
+  return Math.abs(currentTimeInSec - slackRequestTimestampInSec) < FIVE_MIN_IN_SEC;
+}
+
+async function validateSlackRequest(event: ApiGatewayEvent): Promise<boolean> {
+  console.log(JSON.stringify(event.headers, null, 2));
+
+  const requestTimestamp: number = +event.headers["x-slack-request-timestamp"];
+  if (!validateTimestamp(requestTimestamp)) {
+    return false;
+  }
+
+  const signingSecret = await getSigningSecret();
+  const hmac = crypto.createHmac("sha256", signingSecret);
+
+  const requestSignature = event.headers["x-slack-signature"];
+  const [version, slackHash] = requestSignature.split("=");
+
+  const requestBody = qs.stringify(event.body, { format: "RFC1738" });
+  const calculatedSignature = hmac.update(`${version}:${requestTimestamp}:${requestBody}`, "utf8").digest("hex");
+
+  return crypto.timingSafeEqual(Buffer.from(calculatedSignature, "utf8"), Buffer.from(slackHash, "utf8"));
+}
+
+export const handler = async (event: ApiGatewayEvent) => {
+  let body = JSON.parse(event.body);
+
+  // verify request
+  if (!validateSlackRequest(event)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Request was invalid" })
     };
+  }
 
-    return response;
+  // respond with challenge
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify({ challenge: body.challenge })
+  };
+
+  return response;
 };
