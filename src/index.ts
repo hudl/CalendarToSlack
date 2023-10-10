ea62feff77cd0b1b8fe5d6ead57522158eb61df0
Ashlyn Slawnyk <ashlyn.slawnyk@hudl.com>
2023-10-10 17:29:40 -0500

    Update lambda client to new SDK

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import AWS from 'aws-sdk';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 import oauth from 'simple-oauth2';
 import { WebClient } from '@slack/web-api';
 import { getEventsForUser, CalendarEvent, ShowAs } from './services/calendar';
@@ -13,7 +13,6 @@ import {
 } from './services/dynamo';
 import { setUserStatus, setUserPresence, getUserByEmail, postMessage, SlackUser } from './services/slack';
 import { Handler } from 'aws-lambda';
-import { InvocationRequest } from 'aws-sdk/clients/lambda';
 import { getStatusForUserEvent } from './utils/mapEventStatus';
 import { GraphApiAuthenticationProvider } from './services/calendar/graphApiAuthenticationProvider';
 import config from '../config';
@@ -58,24 +57,23 @@ const sendUpcomingEventMessage = async (
 export const update: Handler = async () => {
   const batchSize = 10;
 
-  const lambda = new AWS.Lambda({
+  const lambda = new LambdaClient({
     apiVersion: 'latest',
-    region: 'us-east-1',
-    endpoint: process.env.IS_OFFLINE ? 'http://localhost:3000' : undefined,
+    region: config.region,
+    endpoint: process.env.IS_OFFLINE ? config.hosts.dev : undefined,
   });
-
-  const invokeParams: InvocationRequest = {
-    FunctionName: 'calendar2slack-prod-update-batch',
-    InvocationType: 'Event',
-    LogType: 'None',
-  };
 
   const userSettings = await getAllUserSettings();
 
   for (var i = 0; i < userSettings.length; i += batchSize) {
     const batch = userSettings.slice(i, i + batchSize).map((us) => us.email);
-
-    lambda.invoke({ Payload: JSON.stringify({ emails: batch }), ...invokeParams }).send();
+    const command = new InvokeCommand({
+      FunctionName: 'calendar2slack-prod-update-batch',
+      InvocationType: 'Event',
+      LogType: 'Tail',
+      Payload: JSON.stringify({ emails: batch }),
+    });
+    await lambda.send(command);
   }
 };
 
